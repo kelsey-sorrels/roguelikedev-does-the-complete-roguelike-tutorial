317e15bb75d28bed091833fa5672711cf4a270d8
LDanaee <kercyan@live.fr>
2017-06-28 21:45:47 +0200

    Oublié le render

---
+++ render_function.py
@@ -0,0 +1,24 @@
+import libtcodpy as libtcod
+
+
+def render_all(con, entities, screen_width, screen_height):
+    # Draw all entities in the list
+    for entity in entities:
+        draw_entity(con, entity)
+
+    libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
+
+def clear_all(con, entities):
+    for entity in entities:
+        clear_entity(con, entity)
+
+
+def draw_entity(con, entity):
+    libtcod.console_set_default_foreground(con, entity.color)
+    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+
+
+def clear_entity(con, entity):
+    # erase the character that represents this object
+    libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
